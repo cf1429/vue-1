d3ac3dc54f337a33c57877ef6a7a7deaa1842f75
cf1429 <1429043932@qq.com>
2020-04-05 15:53:08 +0800

    vue 前后端分离项目实践首次提交

---
+++ src/views/Book.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <td>编号</td>
+        <td>图书名称</td>
+        <td>作者</td>
+      </tr>
+      <tr v-for="item in books" :key='item.id'>
+        <td>{{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.author}}</td>
+      </tr>
+    </table>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Book',
+  data () {
+    return {
+      msg: 'hello vue',
+      books: [
+        {
+          id: 1,
+          name: 'java基础',
+          author: '张三'
+        },
+        {
+          id: 2,
+          name: 'veu基础',
+          author: '里三'
+        },
+        {
+          id: 3,
+          name: 'css基础',
+          author: '张柳'
+        }
+      ]
+    }
+  },
+  created () {
+    const _this = this
+    axios.get('http://localhost:8070/api/book/findAll').then(function (res) {
+      _this.books = res.data.data
+      console.log(_this.books)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
